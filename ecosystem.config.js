1b583325cf1d2e50bc9b10b3fb62ba309b299651
razzkumar <rajkumarrai456@gmail.com>
2018-12-27 16:41:06 +0545

    minor fxes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,33 @@
 module.exports = {
-  apps : [{
-    name: 'pm2-learn',
-    script: 'app.js',
+  apps: [
+    {
+      name: "pm2-learn",
+      script: "app.js",
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 2,
-    autorestart: true,
-    watch: true,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      args: "one two",
+      instances: 2,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
     }
-  }],
+  ],
 
-  deploy : {
-    production : {
-      user : 'razz',
-      host : '192.168.88.12',
-      ref  : 'origin/master',
-      repo : 'https://github.com/razzkumar/pm2-test.git',
-      path : '/var/www/http/pm2-test',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  deploy: {
+    production: {
+      user: "razz",
+      host: "192.168.88.12",
+      ref: "origin/master",
+      repo: "https://github.com/razzkumar/pm2-test.git",
+      path: "/var/www/http/pm2-test",
+      "post-deploy":
+        "/home/razz/.nvm/versions/node/v10.15.0/bin install && pm2 reload ecosystem.config.js --env production"
     }
   }
 };
